48fff87d5fa4222bb263a23aadb5f8a0cf0d5df3
Kristovel Adi Sucipto <kristovelas@gmail.com>
2023-01-19 17:10:48 +0700

    Membuat home website

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,118 +6,166 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Editor HTML</title>
-
-    <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="assets/css/bootstrap-4.0.0/dist/css/bootstrap.min.css" rel="stylesheet" />
-
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="assets/fontawesome-free-6.1.1-web/css/all.min.css">
-
-    <!-- Code Mirror -->
-    <link rel="stylesheet" href="assets/codemirror/lib/codemirror.css">
-
-    <!-- Theme Code Mirror-->
-    <link rel="stylesheet" href="assets/codemirror/theme/dracula.css">
-    <link rel="stylesheet" href="assets/codemirror/theme/base16-light.css">
-
+    <title>Landing Page - Start Bootstrap Theme</title>
     <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="assets/sidebar/assets/favicon.ico" />
-
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="assets/startbootstrap-landing-page-gh-pages/css/styles.css" rel="stylesheet" />
 </head>
 
-<body style="background-color: #ABCCFD;">
-    <div class="container">
-        <div class="py-5">
-            <div class="container">
-                <div class="row ml-1">
-                    <div class="mb-2">
-                        <button class="btn btn-primary">Run</button>
-                    </div>
-                    <div class="ml-2">
-                        <a class="btn btn-outline-secondary" data-toggle="tooltip" title="Orientation"><i class="fa-solid fa-rotate"></i></a>
-                    </div>
-                    <select name="theme" id="theme" class="btn btn-sm btn-outline-dark">
-                        <option>Theme</option>
-                        <option value="dracula" class="text-left">Dracula</option>
-                        <option value="base16-light">Base16 Light</option>
-                    </select>
-                </div>
-                <div class="row">
-                    <div class="col-md-6">
-                        <textarea name="textEditor" class="codemirror-textarea" id="textEditor" cols="150" rows="23" class="col-lg-12 col-md"></textarea>
-                    </div>
-                    <div class="col-md-6" aria-disabled="true">
-                        <textarea name="textPreview" class="codemirror-textarea" id="textPreview" cols="150" rows="23" class="col-lg-12 col-md"></textarea>
+<body>
+    <!-- Navigation-->
+    <nav class="navbar navbar-light bg-light static-top">
+        <div class="container">
+            <a class="navbar-brand" href="#!">Start Bootstrap</a>
+            <a class="btn btn-primary" href="#signup">Sign Up</a>
+        </div>
+    </nav>
+    <!-- Masthead-->
+    <header class="masthead">
+        <div class="container position-relative">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <div class="text-center text-white">
+                        <!-- Page heading-->
+                        <h1 class="mb-5">Generate more leads with a professional landing page!</h1>
+                        <!-- Signup form-->
+                        <!-- * * * * * * * * * * * * * * *-->
+                        <!-- * * SB Forms Contact Form * *-->
+                        <!-- * * * * * * * * * * * * * * *-->
+                        <!-- This form is pre-integrated with SB Forms.-->
+                        <!-- To make this form functional, sign up at-->
+                        <!-- https://startbootstrap.com/solution/contact-forms-->
+                        <!-- to get an API token!-->
+                        <form class="form-subscribe" id="contactForm">
+                            <!-- Email address input-->
+                            <div class="row">
+                                <div class="col">
+                                    <input class="form-control form-control-lg" id="emailAddress" type="email" placeholder="Email Address" data-sb-validations="required,email" />
+                                    <div class="invalid-feedback text-white" data-sb-feedback="emailAddress:required">Email Address is required.</div>
+                                    <div class="invalid-feedback text-white" data-sb-feedback="emailAddress:email">Email Address Email is not valid.</div>
+                                </div>
+                                <div class="col-auto"><button class="btn btn-primary btn-lg disabled" id="submitButton" type="submit">Submit</button></div>
+                            </div>
+                            <!-- Submit success message-->
+                            <!---->
+                            <!-- This is what your users will see when the form-->
+                            <!-- has successfully submitted-->
+                            <div class="d-none" id="submitSuccessMessage">
+                                <div class="text-center mb-3">
+                                    <div class="fw-bolder">Form submission successful!</div>
+                                    <p>To activate this form, sign up at</p>
+                                    <a class="text-white" href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+                                </div>
+                            </div>
+                            <!-- Submit error message-->
+                            <!---->
+                            <!-- This is what your users will see when there is-->
+                            <!-- an error submitting the form-->
+                            <div class="d-none" id="submitErrorMessage">
+                                <div class="text-center text-danger mb-3">Error sending message!</div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-
-    <!-- Library Javascript -->
+    </header>
+    <!-- Icons Grid-->
+    <section class="features-icons bg-light text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                        <div class="features-icons-icon d-flex"><i class="bi-window m-auto text-primary"></i></div>
+                        <h3>Fully Responsive</h3>
+                        <p class="lead mb-0">This theme will look great on any device, no matter the size!</p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                        <div class="features-icons-icon d-flex"><i class="bi-layers m-auto text-primary"></i></div>
+                        <h3>Bootstrap 5 Ready</h3>
+                        <p class="lead mb-0">Featuring the latest build of the new Bootstrap 5 framework!</p>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-0 mb-lg-3">
+                        <div class="features-icons-icon d-flex"><i class="bi-terminal m-auto text-primary"></i></div>
+                        <h3>Easy to Use</h3>
+                        <p class="lead mb-0">Ready to use with your own content, or customize the source files!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Image Showcases-->
+    <section class="showcase">
+        <div class="container-fluid p-0">
+            <div class="row g-0">
+                <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('assets/img/bg-showcase-1.jpg')"></div>
+                <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+                    <h2>Fully Responsive Design</h2>
+                    <p class="lead mb-0">When you use a theme created by Start Bootstrap, you know that the theme will look great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!</p>
+                </div>
+            </div>
+            <div class="row g-0">
+                <div class="col-lg-6 text-white showcase-img" style="background-image: url('assets/img/bg-showcase-2.jpg')"></div>
+                <div class="col-lg-6 my-auto showcase-text">
+                    <h2>Updated For Bootstrap 5</h2>
+                    <p class="lead mb-0">Newly improved, and full of great utility classes, Bootstrap 5 is leading the way in mobile responsive web development! All of the themes on Start Bootstrap are now using Bootstrap 5!</p>
+                </div>
+            </div>
+            <div class="row g-0">
+                <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('assets/img/bg-showcase-3.jpg')"></div>
+                <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+                    <h2>Easy to Use & Customize</h2>
+                    <p class="lead mb-0">Landing Page is just HTML and CSS with a splash of SCSS for users who demand some deeper customization options. Out of the box, just add your content and images, and your new landing page will be ready to go!</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Footer-->
+    <footer class="footer bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 h-100 text-center text-lg-start my-auto">
+                    <ul class="list-inline mb-2">
+                        <li class="list-inline-item"><a href="#!">About</a></li>
+                        <li class="list-inline-item">⋅</li>
+                        <li class="list-inline-item"><a href="#!">Contact</a></li>
+                        <li class="list-inline-item">⋅</li>
+                        <li class="list-inline-item"><a href="#!">Terms of Use</a></li>
+                        <li class="list-inline-item">⋅</li>
+                        <li class="list-inline-item"><a href="#!">Privacy Policy</a></li>
+                    </ul>
+                    <p class="text-muted small mb-4 mb-lg-0">&copy; Your Website 2022. All Rights Reserved.</p>
+                </div>
+                <div class="col-lg-6 h-100 text-center text-lg-end my-auto">
+                    <ul class="list-inline mb-0">
+                        <li class="list-inline-item me-4">
+                            <a href="#!"><i class="bi-facebook fs-3"></i></a>
+                        </li>
+                        <li class="list-inline-item me-4">
+                            <a href="#!"><i class="bi-twitter fs-3"></i></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#!"><i class="bi-instagram fs-3"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-
     <!-- Core theme JS-->
-    <script src="assets/css/bootstrap-4.0.0/dist/js/bootstrap.min.js"></script>
-
-    <!-- Jquery -->
-    <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
-
-    <!-- Code Mirror JS -->
-    <script src="assets/codemirror/lib/codemirror.js"></script>
-
-    <!-- Mode Editor -->
-    <script src="assets/codemirror/mode/htmlmixed/htmlmixed.js"></script>
-    <script src="assets/codemirror/mode/xml/xml.js"></script>
-    <!-- <script src="assets/codemirror/mode/javascript/javascript.js"></script> -->
-    <script src="assets/codemirror/mode/php/php.js"></script>
-    <script src="assets/codemirror/mode/clike/clike.js"></script>
-    <script src="assets/codemirror/mode/css/css.js"></script>
-
-    <!-- Addons Code Mirror -->
-    <script src="assets/codemirror/addon/edit/closetag.js"></script>
-    <script src="assets/codemirror/addon/edit/matchbrackets.js"></script>
-
-    <!-- Main JS -->
-    <script src="assets/main.js"></script>
-
-    <!-- Javascript Code -->
-    <script>
-        window.onload = () => {
-            const [input, output] = document.querySelectorAll(".codemirror-textarea");
-            const [run, clear] = document.querySelectorAll("button");
-            const editor = CodeMirror.fromTextArea(input, {
-                lineNumbers: true,
-                theme: 'dracula',
-                matchBrackets: true,
-                mode: "text/php",
-                indentUnit: 4,
-                indentWithTabs: true
-            });
-            editor.setSize("550", "500");
-            const shell = CodeMirror.fromTextArea(output, {
-                lineNumbers: false,
-            });
-            shell.setSize("550", "500");
-
-            run.addEventListener("click", () => {
-                const codeToRun = editor.getValue();
-                try {
-                    shell.replaceRange(eval(`${codeToRun}`) + "\n", CodeMirror.Pos(shell.lastLine()));
-                } catch (e) {
-                    shell.replaceRange(e + "\n", CodeMirror.Pos(shell.lastLine()));
-                }
-            });
-
-            /**Fail 1 */
-            // run.addEventListener("click", () => {
-            //     const codeToRun = editor.getValue();
-            //     shell.setValue(eval(`${codeToRun}`));
-            // });
-        }
-    </script>
+    <script src="assets/startbootstrap-landing-page-gh-pages/js/scripts.js"></script>
+    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
 </body>
 
 </html>
