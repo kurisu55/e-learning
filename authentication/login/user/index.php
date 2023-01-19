6311ca645b109b5a417bba80cd0aa6d88ced4e8e
Kristovel Adi Sucipto <kristovelas@gmail.com>
2023-01-19 17:10:23 +0700

    Menambahkan halaman login user

--- authentication/login/user/index.php
+++ authentication/login/user/index.php
@@ -7,46 +7,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Login - SB Admin</title>
-    <link href="../../assets/startbootstrap-sb-admin-gh-pages/css/styles.css" rel="stylesheet" />
+    <title>Login</title>
+    <link href="../../../assets/startbootstrap-sb-admin-gh-pages/css/styles.css" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
 </head>
 
 <body class="bg-primary">
-    <div id="layoutAuthentication">
-        <div id="layoutAuthentication_content">
-            <main>
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-lg-5">
-                            <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                <div class="card-header">
-                                    <h3 class="text-center font-weight-light my-4">Admin</h3>
-                                </div>
-                                <div class="card-body">
-                                    <form>
-                                        <div class="form-floating mb-3">
-                                            <input class="form-control" id="inputEmail" type="email" placeholder="name@example.com" />
-                                            <label for="inputEmail">Email address</label>
+    <div class="text-center">
+        <div class="row justify-content-md-center">
+            <div class="col align-self-start">
+                <div class="col col-md">
+                    <img class="mt-5" src="../../../assets/img/bg-idx-min.jpg" width="500" height="325" alt="">
+                </div>
+            </div>
+            <div class="col align-self-end">
+                <div id="layoutAuthentication">
+                    <div id="layoutAuthentication_content">
+                        <main>
+                            <div class="">
+                                <div class="col col-md">
+                                    <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                        <div class="card-header">
+                                            <h3 class="text-center font-weight-light my-4">Login</h3>
                                         </div>
-                                        <div class="form-floating mb-3">
-                                            <input class="form-control" id="inputPassword" type="password" placeholder="Password" />
-                                            <label for="inputPassword">Password</label>
+                                        <div class="card-body">
+                                            <form>
+                                                <div class="form-floating mb-3">
+                                                    <input class="form-control" id="inputEmail" type="email" placeholder="name@example.com" />
+                                                    <label for="inputEmail">Email address</label>
+                                                </div>
+                                                <div class="form-floating mb-3">
+                                                    <input class="form-control" id="inputPassword" type="password" placeholder="Password" />
+                                                    <label for="inputPassword">Password</label>
+                                                </div>
+                                                <div class="d-flex align-items-center mt-4 mb-0">
+                                                    <a class="btn btn-primary col-lg" href="index.html">Login</a>
+                                                </div>
+                                            </form>
                                         </div>
-                                        <div class="d-flex align-items-center mt-4 mb-0">
-                                            <a class="btn btn-primary col-lg" href="index.html">Login</a>
-                                        </div>
-                                    </form>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
                     </div>
+                    </main>
                 </div>
-            </main>
+            </div>
         </div>
     </div>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-    <script src="../../assets/startbootstrap-sb-admin-gh-pages/js/scripts.js"></script>
+    <script src="../../../assets/startbootstrap-sb-admin-gh-pages/js/scripts.js"></script>
 </body>
 
 </html>
