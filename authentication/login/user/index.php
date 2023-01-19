dec8b521b6647eac9e1a08db8ab1f69ee5f11da5
Kristovel Adi Sucipto <kristovelas@gmail.com>
2023-01-20 02:10:55 +0700

    Membuat style pada halaman login user

--- authentication/login/user/index.php
+++ authentication/login/user/index.php
@@ -9,52 +9,74 @@
     <meta name="author" content="" />
     <title>Login</title>
     <link href="../../../assets/startbootstrap-sb-admin-gh-pages/css/styles.css" rel="stylesheet" />
+    <link href="../../../assets/style.css" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
 </head>
 
-<body class="bg-primary">
-    <div class="container text-center">
-        <div class="row justify-content-md-center">
-            <div class="col align-self-start">
-                <div class="col col-md">
-                    <img class="mt-5" src="../../../assets/img/bg-idx-min.jpg" width="500" height="325" alt="">
+<body>
+    <section class="vh-100">
+        <div class="container-fluid h-custom">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-md-9 col-lg-6 col-xl-5">
+                    <img src="../../../assets/img/vecteezy_programer-learning-programming-languages-by-computer-laptop_.jpg" class="img-fluid" alt="Sample image">
+                    <div class="small"><a href="https://www.vecteezy.com/free-vector/programming" class="link-dark">Programming Vectors by Vecteezy</a></div>
                 </div>
-            </div>
-            <div class="col align-self-end">
-                <div id="layoutAuthentication">
-                    <div id="layoutAuthentication_content">
-                        <main>
-                            <div class="container">
-                                <div class="col col-md">
-                                    <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                        <div class="card-header">
-                                            <h3 class="text-center font-weight-light my-4">Login</h3>
-                                        </div>
-                                        <div class="card-body">
-                                            <form>
-                                                <div class="form-floating mb-3">
-                                                    <input class="form-control" id="inputEmail" type="email" placeholder="name@example.com" />
-                                                    <label for="inputEmail">Email address</label>
-                                                </div>
-                                                <div class="form-floating mb-3">
-                                                    <input class="form-control" id="inputPassword" type="password" placeholder="Password" />
-                                                    <label for="inputPassword">Password</label>
-                                                </div>
-                                                <div class="d-flex align-items-center mt-4 mb-0">
-                                                    <a class="btn btn-primary col-lg" href="index.html">Login</a>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                    </div>
-                    </main>
+                <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                    <form>
+                        <div class="divider d-flex align-items-center my-4">
+                            <h1 class="text-center fw-bold mx-3 mb-0">Login</h1>
+                        </div>
+
+                        <!-- Email input -->
+                        <div class="form-outline mb-4">
+                            <input type="email" id="form3Example3" class="form-control form-control-lg" placeholder="Enter a valid email address" />
+                            <label class="form-label" for="form3Example3">Email address</label>
+                        </div>
+
+                        <!-- Password input -->
+                        <div class="form-outline mb-3">
+                            <input type="password" id="form3Example4" class="form-control form-control-lg" placeholder="Enter password" />
+                            <label class="form-label" for="form3Example4">Password</label>
+                        </div>
+
+                        <div class="d-flex justify-content-between align-items-center">
+                            <a href="#!" class="text-body">Forgot password?</a>
+                        </div>
+
+                        <div class="text-center text-lg-start mt-4 pt-2">
+                            <button type="button" class="btn btn-primary btn-lg" style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
+                            <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!" class="link-danger">Register</a></p>
+                        </div>
+
+                    </form>
                 </div>
             </div>
         </div>
-    </div>
-    </div>
+        <div class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
+            <!-- Copyright -->
+            <div class="text-white mb-3 mb-md-0">
+                Copyright © 2020. All rights reserved.
+            </div>
+            <!-- Copyright -->
+
+            <!-- Right -->
+            <div>
+                <a href="#!" class="text-white me-4">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="#!" class="text-white me-4">
+                    <i class="fab fa-twitter"></i>
+                </a>
+                <a href="#!" class="text-white me-4">
+                    <i class="fab fa-google"></i>
+                </a>
+                <a href="#!" class="text-white">
+                    <i class="fab fa-linkedin-in"></i>
+                </a>
+            </div>
+            <!-- Right -->
+        </div>
+    </section>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
     <script src="../../../assets/startbootstrap-sb-admin-gh-pages/js/scripts.js"></script>
