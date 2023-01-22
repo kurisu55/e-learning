f4348edaaa417e4b1b925eae5f93778a9387be3c
Kristovel Adi Sucipto <kristovelas@gmail.com>
2023-01-22 07:41:20 +0700

    Coding editor dipecahkan

--- editor/html/index.php
+++ editor/html/index.php
@@ -1,4 +1,9 @@
+<!-- SCRIPT AWAL PHP -->
 <?php
+// Variabel TAG Template
+$title = 'Code Editor HTML';
+$description = '';
+$author = '';
 ?>
 
 <!DOCTYPE html>
@@ -6,54 +11,75 @@
 
 <head>
     <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Editor HTML</title>
-
-    <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="assets/css/bootstrap-4.0.0/dist/css/bootstrap.min.css" rel="stylesheet" />
-
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="assets/fontawesome-free-6.1.1-web/css/all.min.css">
+    <title><?= $title; ?></title>
+    <link href="../../assets/startbootstrap-sb-admin-gh-pages/css/styles.css" rel="stylesheet" />
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
 
     <!-- Code Mirror -->
-    <link rel="stylesheet" href="assets/codemirror/lib/codemirror.css">
+    <link rel="stylesheet" href="../../assets/codemirror/lib/codemirror.css">
 
     <!-- Theme Code Mirror-->
-    <link rel="stylesheet" href="assets/codemirror/theme/dracula.css">
-    <link rel="stylesheet" href="assets/codemirror/theme/base16-light.css">
+    <link rel="stylesheet" href="../../assets/codemirror/theme/dracula.css">
+    <link rel="stylesheet" href="../../assets/codemirror/theme/base16-light.css">
 
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="assets/sidebar/assets/favicon.ico" />
 
+    <!-- Code Mirror JS -->
+    <script src="../../assets/codemirror/lib/codemirror.js"></script>
+
+    <!-- Mode Editor -->
+    <script src="../../assets/codemirror/mode/xml/xml.js"></script>
+    <script src="../../assets/codemirror/mode/javascript/javascript.js"></script>
+    <script src="../../assets/codemirror/mode/css/css.js"></script>
+    <script src="../../assets/codemirror/mode/htmlmixed/htmlmixed.js"></script>
+
+    <!-- Addons Code Mirror -->
+    <script src="../../assets/codemirror/addon/edit/closetag.js"></script>
+    <script src="../../assets/codemirror/addon/edit/matchbrackets.js"></script>
+
+    <style>
+        iframe {
+            width: 100%;
+            float: left;
+            height: 300px;
+            border: 1px solid black;
+        }
+    </style>
 </head>
 
-<body style="background-color: #ABCCFD;">
-    <div class="container">
-        <div class="py-5">
-            <div class="container">
-                <div class="row ml-1">
-                    <div class="mb-2">
-                        <button class="btn btn-primary">Run</button>
-                    </div>
-                    <div class="ml-2">
-                        <a class="btn btn-outline-secondary" data-toggle="tooltip" title="Orientation"><i class="fa-solid fa-rotate"></i></a>
-                    </div>
-                    <select name="theme" id="theme" class="btn btn-sm btn-outline-dark">
-                        <option>Theme</option>
-                        <option value="dracula" class="text-left">Dracula</option>
-                        <option value="base16-light">Base16 Light</option>
-                    </select>
+<body class="">
+    <div class="container" style="margin-top: 100px;">
+        <div class="row mb-3">
+            <span class="col-1">
+                <button type="button" class="btn btn-primary" id="runCode" onclick="buttonRun()">Run</button>
+            </span>
+            <span class="col-1">
+                <a class="btn btn-outline-secondary" data-toggle="tooltip" title="Orientation"><i class="fa-solid fa-rotate"></i></a>
+            </span>
+            <span class="col-1">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-outline-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                        Theme
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">HTML</a></li>
+                        <li><a class="dropdown-item" href="#">PHP</a></li>
+                        <li><a class="dropdown-item" href="#">Javascript</a></li>
+                    </ul>
                 </div>
-                <div class="row">
-                    <div class="col-md-6">
-                        <textarea name="textEditor" class="codemirror-textarea" id="textEditor" cols="150" rows="23" class="col-lg-12 col-md"></textarea>
-                    </div>
-                    <div class="col-md-6" aria-disabled="true">
-                        <textarea name="textPreview" class="codemirror-textarea" id="textPreview" cols="150" rows="23" class="col-lg-12 col-md"></textarea>
-                    </div>
-                </div>
+            </span>
+        </div>
+        <div class=" row">
+            <div class="col-md-6">
+                <textarea id="code" name="code" class="CodeMirror"></textarea>
+            </div>
+            <div class="col-md-6 border">
+                <iframe id="preview" style="overflow: hidden;"></iframe>
             </div>
         </div>
     </div>
@@ -68,58 +94,31 @@
     <!-- Jquery -->
     <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
 
-    <!-- Code Mirror JS -->
-    <script src="assets/codemirror/lib/codemirror.js"></script>
-
-    <!-- Mode Editor -->
-    <script src="assets/codemirror/mode/htmlmixed/htmlmixed.js"></script>
-    <script src="assets/codemirror/mode/xml/xml.js"></script>
-    <!-- <script src="assets/codemirror/mode/javascript/javascript.js"></script> -->
-    <script src="assets/codemirror/mode/php/php.js"></script>
-    <script src="assets/codemirror/mode/clike/clike.js"></script>
-    <script src="assets/codemirror/mode/css/css.js"></script>
-
-    <!-- Addons Code Mirror -->
-    <script src="assets/codemirror/addon/edit/closetag.js"></script>
-    <script src="assets/codemirror/addon/edit/matchbrackets.js"></script>
-
     <!-- Main JS -->
     <script src="assets/main.js"></script>
 
     <!-- Javascript Code -->
     <script>
-        window.onload = () => {
-            const [input, output] = document.querySelectorAll(".codemirror-textarea");
-            const [run, clear] = document.querySelectorAll("button");
-            const editor = CodeMirror.fromTextArea(input, {
-                lineNumbers: true,
-                theme: 'dracula',
-                matchBrackets: true,
-                mode: "text/php",
-                indentUnit: 4,
-                indentWithTabs: true
-            });
-            editor.setSize("550", "500");
-            const shell = CodeMirror.fromTextArea(output, {
-                lineNumbers: false,
-            });
-            shell.setSize("550", "500");
+        var delay;
+        // Initialize CodeMirror editor with a nice html5 canvas demo.
+        var editor = CodeMirror.fromTextArea(document.getElementById('code'), {
+            mode: 'text/html',
+            lineNumbers: true
+        });
+        // editor.on("change", function() {
+        //     clearTimeout(delay);
+        //     delay = setTimeout(updatePreview, 300);
+        // });
 
-            run.addEventListener("click", () => {
-                const codeToRun = editor.getValue();
-                try {
-                    shell.replaceRange(eval(`${codeToRun}`) + "\n", CodeMirror.Pos(shell.lastLine()));
-                } catch (e) {
-                    shell.replaceRange(e + "\n", CodeMirror.Pos(shell.lastLine()));
-                }
-            });
-
-            /**Fail 1 */
-            // run.addEventListener("click", () => {
-            //     const codeToRun = editor.getValue();
-            //     shell.setValue(eval(`${codeToRun}`));
-            // });
+        function buttonRun() {
+            var previewFrame = document.getElementById('preview');
+            var preview = previewFrame.contentDocument || previewFrame.contentWindow.document;
+            preview.open();
+            preview.write(editor.getValue());
+            preview.close();
         }
+        // setTimeout(updatePreview, 300);
+        buttonRun();
     </script>
 </body>
 
