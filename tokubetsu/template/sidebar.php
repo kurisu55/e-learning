d714022301c645ab2549213d737a5ad22d631fcd
Kristovel Adi Sucipto <kristovelas@gmail.com>
2023-01-25 06:37:31 +0700

    Menambahkan tombol edit kuis

--- tokubetsu/template/sidebar.php
+++ tokubetsu/template/sidebar.php
@@ -3,19 +3,24 @@
         <nav class="sb-sidenav accordion sb-sidenav-light" id="sidenavAccordion">
             <div class="sb-sidenav-menu">
                 <div class="nav">
-                    <div class="sb-sidenav-menu-heading">Start</div>
+                    <div class="sb-sidenav-menu-heading">Home</div>
                     <a class="nav-link" href="dashboard.php">
                         <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                         Dashboard
                     </a>
-                    <div class="sb-sidenav-menu-heading">Edit</div>
+                    <div class="sb-sidenav-menu-heading">Materi</div>
                     <a class="nav-link" href="materi.php">
                         <div class="sb-nav-link-icon"><i class="fas fa-book"></i></div>
-                        Materi
+                        Tambah Materi
+                    </a>
+                    <div class="sb-sidenav-menu-heading">Kuis</div>
+                    <a class="nav-link" href="set_kuis.php">
+                        <div class="sb-nav-link-icon"><i class="fas fa-question"></i></div>
+                        Tambah Kuis
                     </a>
                     <a class="nav-link" href="set_kuis.php">
                         <div class="sb-nav-link-icon"><i class="fas fa-question"></i></div>
-                        Set Kuis
+                        Edit Kuis
                     </a>
                 </div>
         </nav>
