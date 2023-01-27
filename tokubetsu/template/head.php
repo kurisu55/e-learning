d76f7f4c6eb3f14b8fa44e7e9d6c80bde11e27fd
Kristovel Adi Sucipto <kristovelas@gmail.com>
2023-01-28 03:56:45 +0700

    Session untuk logout dan menampilkan name selesai

--- tokubetsu/template/head.php
+++ tokubetsu/template/head.php
@@ -25,7 +25,13 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#!">Logout</a></li>
+                        <li>
+                            <p class="dropdown-item"><?php echo $_SESSION['name']; ?></p>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider" />
+                        </li>
+                        <li><a class="dropdown-item" href="../authentication/login/logout.php">Logout</a></li>
                     </ul>
                 </li>
             </ul>
