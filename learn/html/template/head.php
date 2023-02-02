7ecc2af6012bdfef38177081df0dfe811016739d
Kristovel Adi Sucipto <kristovelas@gmail.com>
2023-02-02 20:25:49 +0700

    Memperbaiki tombol login dan session login

--- learn/html/template/head.php
+++ learn/html/template/head.php
@@ -46,7 +46,9 @@
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#!">Logout</a></li>
+                    <p class="dropdown-item"><?= $_SESSION["name"]; ?></p>
+                    <hr class="dropdown-divider">
+                    <li><a class="dropdown-item" href="../../authentication/db/conn_db.php">Logout</a></li>
                 </ul>
             </li>
         </ul>
