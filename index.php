accf621b44ef03ab5b824769c0a3c51346744aad
Kristovel Adi Sucipto <kristovelas@gmail.com>
2023-03-02 02:19:46 +0700

    Tinggal buat fitur live search materi

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
 // Session
 session_start();
+
+// IF untuk bila login tidak sebagai level 1  = 'Admin'
+if (isset($_SESSION["level"]) == 1) {
+    header("location:tokubetsu/dashboard/dashboard.php");
+}
+
+// Connet DB
+require 'authentication/db/conn_db.php'
+
+// Query
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,9 +23,9 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Home Belajar Pemrograman</title>
+    <title>Home Code</title>
     <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="assets/img/terminal-solid.svg" />
     <!-- Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
     <!-- Google fonts-->
@@ -28,16 +40,32 @@ session_start();
     <!-- Navigation-->
     <nav class="navbar navbar-light bg-light static-top">
         <div class="container">
-            <a class="navbar-brand col-9" href="#!">Start Bootstrap</a>
-            <!-- Example single danger button -->
+            <div class="col-8">
+                <a class="navbar-brand btn btn-outline-dark" href="index.php">
+                    <img src="assets/img/terminal-solid.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                    Code
+                </a>
+            </div>
+            <!-- Tutorial Button -->
             <div class="btn-group ms-4">
                 <button type="button" class="btn btn-outline-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                     Tutorial
                 </button>
                 <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="learn/html">HTML</a></li>
-                    <li><a class="dropdown-item" href="learn/php">PHP</a></li>
-                    <li><a class="dropdown-item" href="learn/js">Javascript</a></li>
+                    <li><a class="dropdown-item" href="learn/html">Tutorial HTML</a></li>
+                    <li><a class="dropdown-item" href="learn/php">Tutorial PHP</a></li>
+                    <li><a class="dropdown-item" href="learn/js">Tutorial Javascript</a></li>
+                </ul>
+            </div>
+            <!-- Kuis Button -->
+            <div class="btn-group me-2">
+                <button type="button" class="btn btn-outline-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    Kuis
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="kuis/html">Kuis HTML</a></li>
+                    <li><a class="dropdown-item" href="kuis/php">Kuis PHP</a></li>
+                    <li><a class="dropdown-item" href="kuis/js">Kuis Javascript</a></li>
                 </ul>
             </div>
             <?php if (isset($_SESSION["level"])) {
@@ -65,7 +93,16 @@ session_start();
                             <!-- Email address input-->
                             <div class="row">
                                 <div class="col">
-                                    <input class="form-control form-control-lg" id="" type="search" placeholder="Ketik pencarian disini!" />
+                                    <input class="form-control form-control-lg" id="searchMateri" type="search" placeholder="Ketik pencarian disini!" />
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <div class="border">
+                                                <h5 class="card-title text-dark mt-2 mx-1" style="text-align: initial;">Card title</h5>
+                                                <hr class="border border-dark border-2 opacity-50">
+                                                <p class="card-text text-dark mx-1" style="text-align: left;">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                                 <!-- <div class="col-auto"><button class="btn btn-primary btn-lg" type="submit">Submit</button></div> -->
                             </div>
@@ -81,50 +118,59 @@ session_start();
             <div class="row">
                 <div class="col-lg-4">
                     <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                        <div class="features-icons-icon d-flex"><i class="bi-window m-auto text-primary"></i></div>
-                        <h3>Fully Responsive</h3>
-                        <p class="lead mb-0">This theme will look great on any device, no matter the size!</p>
+                        <div class="features-icons-icon d-flex"><i class="bi-book m-auto text-primary"></i></div>
+                        <h3>Materi yang mudah dipahami</h3>
+                        <p class="lead mb-0">Pelajari materi dengan seksama!</p>
                     </div>
                 </div>
                 <div class="col-lg-4">
                     <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-                        <div class="features-icons-icon d-flex"><i class="bi-layers m-auto text-primary"></i></div>
-                        <h3>Bootstrap 5 Ready</h3>
-                        <p class="lead mb-0">Featuring the latest build of the new Bootstrap 5 framework!</p>
+                        <div class="features-icons-icon d-flex"><i class="bi-pen m-auto text-primary"></i></div>
+                        <h3>Kuis</h3>
+                        <p class="lead mb-0">Kuis yang membantu mengingat tutorial yang sudah dipelajari!</p>
                     </div>
                 </div>
                 <div class="col-lg-4">
                     <div class="features-icons-item mx-auto mb-0 mb-lg-3">
                         <div class="features-icons-icon d-flex"><i class="bi-terminal m-auto text-primary"></i></div>
-                        <h3>Easy to Use</h3>
-                        <p class="lead mb-0">Ready to use with your own content, or customize the source files!</p>
+                        <h3>Cobalah Code Editor</h3>
+                        <p class="lead mb-0">Praktek langsung dengan code editor kami!</p>
                     </div>
                 </div>
             </div>
         </div>
     </section>
     <!-- Image Showcases-->
-    <section class="showcase">
+    <section class="showcase mt-5">
         <div class="container-fluid p-0">
             <div class="row g-0">
-                <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('assets/img/bg-showcase-1.jpg')"></div>
+                <div class="col-lg-6 order-lg-2 text-white showcase-img">
+                    <img src="assets/img/htmlpng.png" class=" mt-5 mx-5" alt="" width="400" height="400">
+                </div>
                 <div class="col-lg-6 order-lg-1 my-auto showcase-text">
-                    <h2>Fully Responsive Design</h2>
-                    <p class="lead mb-0">When you use a theme created by Start Bootstrap, you know that the theme will look great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!</p>
+                    <h2>HTML</h2>
+                    <p class="lead mb-0">HTML merupakan bahasa utama dalam pembuatan website!</p>
+                    <a href="learn/html/index.php" class="btn btn-lg btn-outline-danger mt-3">Pelajari</a>
                 </div>
             </div>
             <div class="row g-0">
-                <div class="col-lg-6 text-white showcase-img" style="background-image: url('assets/img/bg-showcase-2.jpg')"></div>
+                <div class="col-lg-6 text-white showcase-img">
+                    <img src="assets/img/jspng.png" class="mt-5 mx-5" alt="" width="400" height="400">
+                </div>
                 <div class="col-lg-6 my-auto showcase-text">
-                    <h2>Updated For Bootstrap 5</h2>
-                    <p class="lead mb-0">Newly improved, and full of great utility classes, Bootstrap 5 is leading the way in mobile responsive web development! All of the themes on Start Bootstrap are now using Bootstrap 5!</p>
+                    <h2>Javascript</h2>
+                    <p class="lead mb-0">Javascript merupakan bahasa pemrograman client-side yang membuat website lebih dinamis!</p>
+                    <a href="learn/js/index.php" class="btn btn-lg btn-outline-warning mt-3">Pelajari</a>
                 </div>
             </div>
             <div class="row g-0">
-                <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('assets/img/bg-showcase-3.jpg')"></div>
+                <div class="col-lg-6 order-lg-2 text-white showcase-img">
+                    <img src="assets/img/phppng.png" class="mt-5 mx-5" alt="" width="550" height="300">
+                </div>
                 <div class="col-lg-6 order-lg-1 my-auto showcase-text">
-                    <h2>Easy to Use & Customize</h2>
-                    <p class="lead mb-0">Landing Page is just HTML and CSS with a splash of SCSS for users who demand some deeper customization options. Out of the box, just add your content and images, and your new landing page will be ready to go!</p>
+                    <h2>PHP</h2>
+                    <p class="lead mb-0">PHP merupakan bahasa pemrograman server-side yang sering dipakai banyak orang!</p>
+                    <a href="learn/php/index.php" class="btn btn-lg btn-outline-info mt-3">Pelajari</a>
                 </div>
             </div>
         </div>
@@ -143,7 +189,7 @@ session_start();
                         <li class="list-inline-item">⋅</li>
                         <li class="list-inline-item"><a href="#!">Privacy Policy</a></li>
                     </ul>
-                    <p class="text-muted small mb-4 mb-lg-0">&copy; Your Website 2023. All Rights Reserved.</p>
+                    <p class="text-muted small mb-4 mb-lg-0">&copy; Code 2023. All Rights Reserved.</p>
                 </div>
             </div>
         </div>
