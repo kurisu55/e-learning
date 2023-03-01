9fb43de439bd51574c04c2e5872f3c227bf43008
Kristovel Adi Sucipto <kristovelas@gmail.com>
2023-03-02 02:05:56 +0700

    main.js tidak terpakai

--- assets/main.js
+++
@@ -1,9 +0,0 @@
-// window.onload = () => {
-//     const [input, output] = document.querySelectorAll(".codemirror-textarea");
-//     const editor = CodeMirror.fromTextArea(input, {
-//         lineNumbers: true
-//     });
-//     const shell = CodeMirror.fromTextArea(output, {
-//         lineNumbers: false
-//     });
-// }
