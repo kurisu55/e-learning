9e55209ca512c2f08bd6fa94d9131daec4178aa6
Kristovel Adi Sucipto <kristovelas@gmail.com>
2023-03-22 17:51:44 +0700

    Membuat data materi untuk live search input

---
+++ data-materi.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'authentication/db/conn_db.php';
+
+$keyword = $_POST["keyword"];
+
+$result = mysqli_query($conn, "SELECT * FROM materi WHERE isi OR judul LIKE '%$keyword%'");
+
+if (empty($keyword)) {
+    echo "";
+} elseif (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+?>
+        <a class="list-group-item list-group-item-action" href="<?= $row["href"] . $row["page"]; ?>.php" style="text-align: left;">
+            <strong class="card-title text-dark mt-2"><u><?= htmlspecialchars_decode($row["judul"]); ?></u></strong>
+            <span class="text-dark disabled">
+                <?= htmlspecialchars_decode($row["isi"]); ?>
+            </span>
+        </a>
+<?php
+    }
+} else {
+    echo "
+    <div class='alert alert-danger' role='alert'>
+    Keyword tidak tersedia!
+    </div>
+    ";
+}
+?>
