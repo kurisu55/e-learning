1072e57c739da5e0a51100c985156896e936a32e
Kristovel Adi Sucipto <kristovelas@gmail.com>
2023-03-22 17:59:25 +0700

    Ubah form input menjadi button code editor

--- learn/js/template/head.php
+++ learn/js/template/head.php
@@ -27,15 +27,10 @@
         <a class="navbar-brand ps-3 text-center" href="../../index.php"><i class="fas fa-terminal"></i> Code</a>
         <!-- Sidebar Toggle-->
         <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
-        <!-- Navbar Search-->
-        <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-            <div class="input-group">
-                <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-                <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
-            </div>
-        </form>
-        <button class="btn btn-outline-primary me-3"><i class="fas fa-rotate"></i></button>
-        <!-- Example single danger button -->
+        <!-- Code Editor Button-->
+        <div class="d-none d-md-inline-block ms-auto me-0 me-md-3 my-2 my-md-0">
+            <a href="../../editor/js/index.php" class="btn btn-warning"><i class="fas fa-laptop-code me-2"></i>Coba JS</a>
+        </div>
         <div class="btn-group me-2">
             <button type="button" class="btn btn-outline-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Mode Tutorial</button>
             <ul class="dropdown-menu">
