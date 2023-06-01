6b8fd4360ecf99471e8c3b8c74893eba0fba8c7e
Kristovel Adi Sucipto <kristovelas@gmail.com>
2023-06-01 15:32:03 +0700

    Update : Ubah query menampilkan materi berdasarkan isi, penambahan title

--- data-materi.php
+++ data-materi.php
@@ -3,15 +3,15 @@ require_once 'authentication/db/conn_db.php';
 
 $keyword = $_POST["keyword"];
 
-$result = mysqli_query($conn, "SELECT * FROM materi WHERE isi OR judul LIKE '%$keyword%'");
+$result = mysqli_query($conn, "SELECT * FROM materi WHERE isi LIKE '%$keyword%'");
 
 if (empty($keyword)) {
     echo "";
 } elseif (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
 ?>
-        <a class="list-group-item list-group-item-action" href="<?= $row["href"] . $row["page"]; ?>.php" style="text-align: left;">
-            <strong class="card-title text-dark mt-2"><u><?= htmlspecialchars_decode($row["judul"]); ?></u></strong>
+        <a class="list-group-item list-group-item-action mb-1" href="<?= $row["href"] . $row["page"]; ?>.php" style="text-align: left;" title="<?= htmlspecialchars_decode($row["judul"]); ?>">
+            <strong class="card-title text-danger mt-2"><u><?= htmlspecialchars_decode($row["judul"]); ?></u></strong>
             <span class="text-dark disabled">
                 <?= htmlspecialchars_decode($row["isi"]); ?>
             </span>
